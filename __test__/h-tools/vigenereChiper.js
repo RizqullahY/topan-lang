995b8957462b92b33b9880cfadccea24fdbafa60
Rizqullah Yusuf <rizqullahyusufrafly@gmail.com>
2025-01-14 14:25:18 +0700

    CHORE: fix the vigenere chiper

--- __test__/h-tools/vigenereChiper.js
+++ __test__/h-tools/vigenereChiper.js
@@ -1,12 +1,21 @@
-// node __test__/h-tools/vigenereChiper.js
-import {VigenereDecrypt, VigenereEncrypt} from "../../lib/h-tools/cryptographic/vigenere-chiper.js";
-
+import { VigenereDecrypt, VigenereEncrypt } from "../../lib/h-tools/cryptographic/vigenere-chiper.js";
 
 let message = 'audzubilla himinas viko nirrojim';
-let key = 'laptopmajapahit'
+let key = 'laptopmajapahit';
+let wrongKey = 'laptopgaming';
 
-// encrypt message using vigenere
+console.log('\nkey       : ', key);
+console.log('plain msg : ', message);
 
-console.log()
-console.log('key       : ', key)
-console.log('plain msg : ' ,message)
+// Encrypt message using vigenere chiper
+let encryptedMessage = VigenereEncrypt(message, key);
+console.log('\nEncrypted message                  :', encryptedMessage);
+
+// Decyrpt message using vigenere chiper and correct key | same key for usign encryption
+let decryptedMessage = VigenereDecrypt(encryptedMessage, key);
+console.log('Decrypted message with correct key :', decryptedMessage);
+
+
+// Decrypt encrypted message with wrong key
+let decryptedMessageWithWrongKey = VigenereDecrypt(encryptedMessage, wrongKey);
+console.log('Decrypted message with wrong key   :', decryptedMessageWithWrongKey);
